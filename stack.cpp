0d813c12283571616d2998d9eeb893f27cd06b00
SagaTheGreat <ajay.patel305@gmail.com>
2019-07-02 17:43:10 +0530

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -4,6 +4,7 @@ Template stack implementation using std::vector.
 
 #include <iostream>
 #include <vector>
+#include <stdexcept>
 
 using namespace std;
 
@@ -14,6 +15,7 @@ public:
 	void push(T val);
 	T pop();
 	bool isEmpty() const;
+    T top() const;
 private:
 	vector<T> stackObj;
 };
@@ -33,9 +35,10 @@ T Stack<T>::pop()
         stackObj.pop_back();
         return temp;
     }
-    cout<<"Stack is empty"<<endl;
-    exit(-1);
- 
+    else
+    {
+        throw std::out_of_range("Stack<>::pop(): empty stack");
+    }
 }
 
 template<typename T>
@@ -44,9 +47,20 @@ bool Stack<T>::isEmpty() const
     return stackObj.empty();
 }
 
+template<typename T>
+T Stack<T>::top () const
+{
+    if (stackObj.empty()) 
+    {
+    throw std::out_of_range("Stack<>::top(): empty stack");
+    }
+    return stackObj.back();
+}
+
 int main()
 {
     Stack<int> s;
+    //cout<<s.pop()<<endl;
     s.push(1);
     s.push(2);
     cout<<s.pop()<<endl;
