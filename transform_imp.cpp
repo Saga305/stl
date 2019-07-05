2313e7503ba6298dad7c37fa04f08c9d18052411
SagaTheGreat <ajay.patel305@gmail.com>
2019-07-05 18:07:13 +0530

    Create transform_imp.cpp

---
+++ transform_imp.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int increase (int i) { 
+    return ++i; 
+}
+
+template<typename T1, typename T2, typename T3>
+void transform(T1 itb, T1 ite, T2 sitb, T3 func)
+{
+
+    while(itb != ite)
+        {
+             *sitb = func(*itb);  
+             ++sitb;
+             ++itb;
+        }
+ }
+
+
+int main()
+{
+    vector<int> vi,iv;
+    
+    for (int i=0; i<5; i++)
+    {
+        vi.push_back(i);
+    }
+    
+    iv.resize(vi.size());
+    
+    transform(vi.begin(),vi.end(),iv.begin(),increase);
+    
+     for (int i=0; i<5; i++)
+    {
+        cout<<iv[i]<<endl;
+    }
+    
+    return 0;
+}
